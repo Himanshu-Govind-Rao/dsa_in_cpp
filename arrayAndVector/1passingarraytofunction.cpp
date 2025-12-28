e87fc079510d05f47a2cafc8b4d6c47363cc8e54
Himanshu-Govind-Rao <himanshugovindrao13@gmail.com>
2025-12-28 10:40:11 +0530

    Implement array display and modification functions
    Added functions to display and modify an array.

--- arrayAndVector/1passingarraytofunction.cpp
+++ arrayAndVector/1passingarraytofunction.cpp
@@ -1,1 +1,18 @@
-
+#include<iostream>
+using namespace std;
+void display(int a[]){
+    for(int i=0; i<5; i++) cout<<a[i]<<" ";
+    cout<<endl;
+    return;
+}
+void change(int b[]){
+    b[0] = 100;
+}
+int main(){
+    int arr[5] = {1,2,4,5,7};
+    // accessing the elements of array in another function 
+    // updation , pass by value / reference ?
+    display(arr);
+    change(arr);
+    display(arr);
+}
