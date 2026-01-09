d97c9b360b78c3a53d7f4deba3976228813c7405
Himanshu-Govind-Rao <himanshugovindrao13@gmail.com>
2026-01-09 21:20:30 +0530

    Change array initialization syntax in 1Decleration.cpp
    Updated array initialization for clarity.

--- array2D/1Decleration.cpp
+++ array2D/1Decleration.cpp
@@ -1,1 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int arr[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int arr[3][3] = {1,2,3,4,5,6,7,8,9};
+    // row -> 3  0-2
+    // column -> 3  0-2
+    cout<<arr[1][1];
+}
 
